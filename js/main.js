4a790e27d27c6b5cf81661585be3bd9b48ee82b3
Khoa Nguen <visthetic@gmail.com>
2018-05-22 20:21:35 -0700

    Adding language switcher functionality

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+/*
+* CUSTOM SELECT
+* @parameter: This adds functionality for a custom select
+*
+* Notes: Lorem ipsum justo vel
+* */
+
+$('select').each(function(){
+  var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+  $this.addClass('select-hidden');
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select-styled"></div>');
+
+  var $styledSelect = $this.next('div.select-styled');
+  $styledSelect.text($this.children('option').eq(0).text());
+
+  var $list = $('<ul />', {
+    'class': 'select-options'
+  }).insertAfter($styledSelect);
+
+  for (var i = 0; i < numberOfOptions; i++) {
+    $('<li />', {
+      text: $this.children('option').eq(i).text(),
+      rel: $this.children('option').eq(i).val()
+    }).appendTo($list);
+  }
+
+  var $listItems = $list.children('li');
+
+  $styledSelect.click(function(e) {
+    e.stopPropagation();
+    $('div.select-styled.active').not(this).each(function(){
+      $(this).removeClass('active').next('ul.select-options').hide();
+    });
+    $(this).toggleClass('active').next('ul.select-options').toggle();
+  });
+
+  $listItems.click(function(e) {
+    e.stopPropagation();
+    $styledSelect.text($(this).text()).removeClass('active');
+    $this.val($(this).attr('rel'));
+    $list.hide();
+    // console.log($this.val());
+    if($this.val() === 'us') {
+      window.location = "https://us.centricwear.com";
+    } else {
+      window.location = "https://vn.centricwear.com";
+    }
+  });
+
+  $(document).click(function() {
+    $styledSelect.removeClass('active');
+    $list.hide();
+  });
+
+});
